fc9327ce2a863818c2e63d84629e69a334b225de
Akanksha1212 <singhakanksha221b@gmail.com>
2022-01-30 12:46:53 +0530

    Speech to text added

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:talk/screens/home.dart';
+import 'package:talk/screens/speechToText.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'HealthQuest',
+    initialRoute: '/',
+    onGenerateRoute: (settings) {
+      switch (settings.name) {
+        case '/':
+          return MaterialPageRoute(builder: (context) => MyApp());
+        case '/home':
+          return MaterialPageRoute(builder: (context) => Home());
+        case '/speectToText':
+          return MaterialPageRoute(builder: (context) => SpeechScreen());
+      }
+    },
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 4),
+      () => Navigator.pushReplacementNamed(context, '/home'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+
+        decoration: BoxDecoration(color: Color(0xffFBFFE2)),
+        // child: Image.asset('assets/images/logo.png'),
+        height: double.infinity,
+        child: Image(
+          image: AssetImage("assets/logo.png"),
+        ),
+      ),
+    );
+  }
+}
